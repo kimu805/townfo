9716ffdba2eedf3e0ab2f5e136c15d9102a8c0ec
kimu805 <kimukimuok805@gmail.com>
2024-09-07 14:13:42 +0900

    create tables of memberships and pending_memberships

--- db/migrate/20240907051035_create_pending_memberships.rb
+++ db/migrate/20240907051035_create_pending_memberships.rb
@@ -1,7 +1,8 @@
 class CreatePendingMemberships < ActiveRecord::Migration[7.0]
   def change
     create_table :pending_memberships do |t|
-
+      t.references :group, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
