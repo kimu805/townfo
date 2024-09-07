83a018a5d7fa1293edbab2c5357ed12a2e77621e
kimu805 <kimukimuok805@gmail.com>
2024-09-07 14:48:11 +0900

    set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "home#index"
-  resources :groups
+  resources :groups do
+    resources :memberships, only: [:create, :destroy]
+    resources :pending_memberships, only: [:new, :create, :destroy]
+  end
 end
