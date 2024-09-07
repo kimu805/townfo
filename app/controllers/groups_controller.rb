51f6fbb7f18abe4dcb60ee0d812ebf26e039e918
kimu805 <kimukimuok805@gmail.com>
2024-09-07 12:35:44 +0900

    add flash message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group, only: [:edit, :update, :destroy, :show]
+  before_action :ensure_owner, only: [:edit, :update, :destroy]
 
   def index
   end
@@ -13,7 +14,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
-      redirect_to root_path
+      redirect_to root_path, notice: "グループ「#{@group.name}」を作成しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,7 +25,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path
+      redirect_to root_path, notice: "グループ「#{@group.name}」を編集しました。"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -32,7 +33,7 @@ class GroupsController < ApplicationController
 
   def destroy
     @group.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: "グループ「#{@group.name}」を削除しました。"
   end
 
   def show
@@ -45,5 +46,12 @@ class GroupsController < ApplicationController
 
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def ensure_owner
+    set_group
+    unless current_user.id == @group.owner_id
+      redirect_to root_path, alert: "アクセス権限がありません。"
+    end
   end
 end
