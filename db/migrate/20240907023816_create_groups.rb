faa986c28f063798fee205e87793b76729d6785d
kimu805 <kimukimuok805@gmail.com>
2024-09-07 11:41:34 +0900

    create table of groups

--- db/migrate/20240907023816_create_groups.rb
+++ db/migrate/20240907023816_create_groups.rb
@@ -1,7 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-
+      t.string  :name,        null: false
+      t.text    :description, null: false
+      t.integer :owner_id,    null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_07_014445) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_07_023816) do
+  create_table "groups", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
