11cedc79343d897da4a09097531fae2f8da5e5a0
kimu805 <kimukimuok805@gmail.com>
2024-09-07 12:09:34 +0900

    add new and create action to group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,31 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :description, owner_id: current_user.id)
   end
 end
