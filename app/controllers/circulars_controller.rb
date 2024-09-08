80fcbb9e26a8017829bd77ae99d3ce97ae5101ac
kimu805 <kimukimuok805@gmail.com>
2024-09-08 18:57:09 +0900

    add action to circulars_controller

--- app/controllers/circulars_controller.rb
+++ app/controllers/circulars_controller.rb
@@ -1,10 +1,51 @@
 class CircularsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group
+  before_action :set_circular, only: [:show, :destroy]
+  before_action :ensure_owner, except: [:index, :show]
+
   def index
+    @circulars = @group.circulars
   end
 
   def new
+    @circulars = @group.circulars.build()
+  end
+
+  def create
+    @circulars = @group.circulars.build(circular_params)
+    if @circulars.save
+      redirect_to root_path, notice: "回覧板を投稿しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  def destroy
+    @circular.destroy
+    redirect_to root_path
+  end
+
+  private
+  def circular_params
+    params.require(:circular).permit(:title, :pdf_file).merge(group_id: @group.id, user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def ensure_owner
+    set_group
+    unless current_user.id == @group.owner_id
+      redirect_to root_path, alert: "アクセス権限がありません。"
+    end
+  end
+
+  def set_circular
+    @circular = @group.circulars.find(params[:id])
   end
 end
