d99281f93b4cc6baf7fb9fd8de72dc658d88e2ac
kimu805 <kimukimuok805@gmail.com>
2024-09-08 15:51:20 +0900

    add index action to memberships_controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,13 @@
 class MembershipsController < ApplicationController
+  before_action :set_group
+
+  def index
+    @users = @group.users
+    @pending_memberships = @group.pending_memberships 
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
