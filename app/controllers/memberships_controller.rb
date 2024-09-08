3bab32be793510b099dfd3bbf12a5df6fd0f5244
kimu805 <kimukimuok805@gmail.com>
2024-09-08 16:04:11 +0900

    Update memberships_controller.rb

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,7 @@
 class MembershipsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_group
+  before_action :ensure_owner
 
   def index
     @users = @group.users
@@ -9,5 +11,12 @@ class MembershipsController < ApplicationController
   private
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def ensure_owner
+    set_group
+    unless current_user.id == @group.owner_id
+      redirect_to root_path, alert: "アクセス権限がありません。"
+    end
   end
 end
