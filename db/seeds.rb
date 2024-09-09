2b5958cf1c32807e6c847dc71ba5f03f49bbd3bd
kimu805 <kimukimuok805@gmail.com>
2024-09-09 14:42:12 +0900

    correct

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(nickname: "owner", email: "owner@owner.com", password: "111111", password_confirmation: "111111")
-User.create(nickname: "test1", email: "test1@test.com", password: "111111", password_confirmation: "111111")
-User.create(nickname: "test2", email: "test2@test.com", password: "111111", password_confirmation: "111111")
-
+# ownerユーザー
 10.times do |i|
-  Group.create(name: "group#{i + 1}", description: "Description of #{i + 1}", owner_id: 1)
-  Membership.create(user_id: 1, group_id: i + 1)
+  User.create(nickname: "owner#{i + 1}", email: "owner#{i + 1}@owner.com", password: "111111", password_confirmation: "111111")
+  Group.create(name: "group#{i + 1}", description: "Description of #{i + 1}", owner_id: i + 1)
+  Membership.create(user_id: i + 1, group_id: i + 1)
 end
 
-5.times do |i|
+# testユーザー
+10.times do |i|
+  User.create(nickname: "test#{i + 1}", email: "test#{i + 1}@test.com", password: "111111", password_confirmation: "111111")
+  Membership.create(user_id: i + 11, group_id: i + 1)
+end
+
+3.times do |i|
   Notice.create(title: "title#{i+1}", content: "content of title#{i+1}", tag_id: 2 , user_id: 1, group_id: 1)
+end
+
+2.times do |i|
+  Notice.create(title: "title#{i+4}", content: "content of title#{i+4}", tag_id: 4 , user_id: 11, group_id: 1)
 end
