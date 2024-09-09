509be6b6b12da271336650ed4942ef1c19e9786b
kimu805 <kimukimuok805@gmail.com>
2024-09-09 13:54:54 +0900

    add validation

--- app/models/circular.rb
+++ app/models/circular.rb
@@ -2,4 +2,9 @@ class Circular < ApplicationRecord
   belongs_to :group
   belongs_to :user
   has_one_attached :pdf_file
+
+  with_options presence: true do
+    validates :title
+    validates :pdf_file
+  end
 end
