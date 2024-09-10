46d272d1e5e3c018712106dd7f45c16adbf14529
kimu805 <kimukimuok805@gmail.com>
2024-09-10 12:02:09 +0900

    Update circular_spec.rb

--- spec/models/circular_spec.rb
+++ spec/models/circular_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Circular, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @circular = FactoryBot.build(:circular)
+  end
+
+  describe "回覧板作成機能" do
+    context "作成できる場合" do
+      it "title, pdf_file, user, groupがあれば作成できる" do
+        expect(@circular).to be_valid
+      end
+    end
+    context "作成できない場合" do
+      it "titleが空では作成できない" do
+        @circular.title = ""
+        @circular.valid?
+        expect(@circular.errors.full_messages).to include("Title can't be blank")
+      end
+      it "pdf_fileが空では作成できない" do
+        @circular.pdf_file = ""
+        @circular.valid?
+        binding.pry
+        expect(@circular.errors.full_messages).to include("pdf_file can't be blank")
+      end
+    end
+  end
 end
