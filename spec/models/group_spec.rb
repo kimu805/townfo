1a2315676409b7f73488da794313fba622aacc55
kimu805 <kimukimuok805@gmail.com>
2024-09-10 11:31:28 +0900

    Update group_spec.rb

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @group = FactoryBot.build(:group, owner_id: @user.id)
+  end
+
+  describe "グループ新規作成機能" do
+    context "新規作成できる場合" do
+      it "name, description, owner_idがあれば新規作成できる" do
+        expect(@group).to be_valid
+      end
+    end
+
+    context "新規作成できない場合" do
+      it "nameが空では登録できない" do
+        @group.name = ""
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Name can't be blank")
+      end
+      it "descriptionが空では登録できない" do
+        @group.description = ""
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Description can't be blank")
+      end
+      it "owner_idが空では登録できない" do
+        @group.owner_id = ""
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Owner can't be blank")
+      end
+
+    end
+  end
 end
