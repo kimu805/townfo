439dc4b7a201a29c07ab8254da283b961c2017b0
kimu805 <kimukimuok805@gmail.com>
2024-09-13 14:35:57 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = current_group.users.find(params[:id])
+    @notices = @user.notices
   end
 end
