1da8676c5eb1d533636a5b508f95f6482c14ab45
kimu805 <kimukimuok805@gmail.com>
2024-09-17 17:33:56 +0900

    add user#edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group
 
   def show
     @user = current_group.users.find(params[:id])
     @notices = @user.notices
+  end
+
+  def edit
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
