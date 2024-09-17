dde4cfc5236edd81edd0db0bf2174d6bacb7ee0d
kimu805 <kimukimuok805@gmail.com>
2024-09-17 13:03:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :groups do
     resources :users, only: :show
-    resources :memberships, only: [:index, :destroy]
+    resources :memberships, only: [:destroy]
     resources :pending_memberships, only: [:create] do
       member do
         patch "approve"
