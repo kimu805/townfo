f0fd1f677fe1d068c65303b68bc06d3e359b7429
kimu805 <kimukimuok805@gmail.com>
2024-09-17 13:03:18 +0900

    Update memberships_controller.rb

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,11 +1,12 @@
 class MembershipsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group
-  before_action :ensure_owner
+  before_action :ensure_owner_or_myself
 
-  def index
-    @users = @group.users
-    @pending_memberships = @group.pending_memberships 
+  def destroy
+    membership_user = Membership.find(params[:id])
+    membership_user.destroy
+    redirect_to group_path(@group)
   end
 
   private
@@ -13,9 +14,9 @@ class MembershipsController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
-  def ensure_owner
+  def ensure_owner_or_myself
     set_group
-    unless current_user.id == @group.owner_id
+    unless current_user.id == @group.owner_id || Membership.find(params[:id]).user == current_user
       redirect_to root_path, alert: "アクセス権限がありません。"
     end
   end
