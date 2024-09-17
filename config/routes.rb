109323aa3f925fc2e8791176bd799b2fbe28a577
kimu805 <kimukimuok805@gmail.com>
2024-09-17 17:15:01 +0900

    Revert "Start implementing user editing function"
    This reverts commit e2b7de5bc8075846e6b8dd7bb790dbc7730bbd94.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users
   
   root to: "home#index"
   resources :groups do
