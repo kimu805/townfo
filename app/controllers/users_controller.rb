09f8a30d5b371a3893d18e74b04fe54b7340ac5b
kimu805 <kimukimuok805@gmail.com>
2024-09-19 15:05:03 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to group_user_path(group_id: current_group.id, id: current_user.id)
+      redirect_to group_user_path(group_id: current_group.id, id: current_user.id), notice: "「#{current_user.nickname}」様の情報を変更しました。"
     else
       render :edit, status: :unprocessable_entity
     end
