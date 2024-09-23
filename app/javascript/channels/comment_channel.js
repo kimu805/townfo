5ec3e79e71683b35571adec6dc4e3c2d06e4e51b
kimu805 <kimukimuok805@gmail.com>
2024-09-23 14:35:24 +0900

    Added a process to run the function only on the notification details page

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,15 +1,18 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if (location.pathname.match(/\/notices\/\d/)) {
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+    }
+  });
+}
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
