22d929264cb4f9b0089f481dd67e52663d1d790c
kimu805 <kimukimuok805@gmail.com>
2024-09-23 12:06:06 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to group_notice_path(group_id: current_group.id, id: params[:notice_id])
+    else
+      render "notice/show"
+    end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:text).merge(notice_id: params[:notice_id])
+  end
 end
