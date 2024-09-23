11c11016df67f45562d2c4f3b0ea0fbe53c235c1
kimu805 <kimukimuok805@gmail.com>
2024-09-23 15:17:17 +0900

    Update comment_channel.js

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,25 +1,30 @@
 import consumer from "channels/consumer"
 
 if (location.pathname.match(/\/notices\/\d/)) {
-  consumer.subscriptions.create("CommentChannel", {
+
+  consumer.subscriptions.create({
+    channel: "CommentChannel",
+    notice_id: location.pathname.match(/\d+/)[0]
+  }, {
+    
     connected() {
       // Called when the subscription is ready for use on the server
     },
-  
+
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
-  
+
     received(data) {
-      const html = `
-      <div class= "noticeShow_comment_box">
-        <div class= "noticeShow_comment_userNickname">
-          ${data.user.nickname}：
+	    const html = `
+        <div class= "noticeShow_comment_box">
+          <div class= "noticeShow_comment_userNickname">
+            ${data.user.nickname} ：
+          </div>
+          <div class= "noticeShow_comment_text">
+            ${data.comment.text}
+          </div>
         </div>
-        <div class= "noticeShow_comment_text">
-          ${data.comment.text}
-        </div>
-      </div>
       `
       const comments = document.querySelector(".noticeShow_commentsIndex_box")
       comments.insertAdjacentHTML("beforeend", html)
@@ -28,4 +33,3 @@ if (location.pathname.match(/\/notices\/\d/)) {
     }
   });
 }
-
