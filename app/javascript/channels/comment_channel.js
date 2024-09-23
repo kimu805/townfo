8407b6a5df280008b38e4334f20273f3627003ce
kimu805 <kimukimuok805@gmail.com>
2024-09-23 15:55:45 +0900

    push

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,30 +1,25 @@
 import consumer from "channels/consumer"
 
 if (location.pathname.match(/\/notices\/\d/)) {
-
-  consumer.subscriptions.create({
-    channel: "CommentChannel",
-    notice_id: location.pathname.match(/\d+/)[0]
-  }, {
-    
+  consumer.subscriptions.create("CommentChannel", {
     connected() {
       // Called when the subscription is ready for use on the server
     },
-
+  
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
-
+  
     received(data) {
-	    const html = `
-        <div class= "noticeShow_comment_box">
-          <div class= "noticeShow_comment_userNickname">
-            ${data.user.nickname} ：
-          </div>
-          <div class= "noticeShow_comment_text">
-            ${formatNewlines(data.comment.text)}
-          </div>
+      const html = `
+      <div class= "noticeShow_comment_box">
+        <div class= "noticeShow_comment_userNickname">
+          ${data.user.nickname}：
         </div>
+        <div class= "noticeShow_comment_text">
+          ${data.comment.text}
+        </div>
+      </div>
       `
       const comments = document.querySelector(".noticeShow_commentsIndex_box")
       comments.insertAdjacentHTML("beforeend", html)
@@ -34,6 +29,3 @@ if (location.pathname.match(/\/notices\/\d/)) {
   });
 }
 
-function formatNewlines(text) {
-  return text.replace(/\n/g, "<br>")
-}
