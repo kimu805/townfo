2dc024253a9602af23ce0f4c27e94ddebad0b380
kimu805 <kimukimuok805@gmail.com>
2024-09-23 14:55:49 +0900

    Update comment_channel.rb

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,6 +1,7 @@
 class CommentChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "comment_channel"
+    @notice = Notice.find(params[:notice_id])
+    stream_for @notice
   end
 
   def unsubscribed
