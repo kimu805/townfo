159e44342d103f470fb7adad8f6f39817c456643
kimu805 <kimukimuok805@gmail.com>
2024-10-18 17:56:16 +0900

    Update count.js

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,4 +1,18 @@
+function countNotice() {
+  const noticeTitleForm = document.getElementById("noticeTitle_form")
+  const noticeTitleCount = document.getElementById("noticeTitle_count")
 
+  if (!noticeTitleForm) return null
+  console.log("読み込み完了")
 
-window.addEventListener("turbo:load", count)
-window.addEventListener("turbo:render", count)
+  noticeTitleForm.addEventListener("input", () => {
+    let titleLength = noticeTitleForm.value.length
+    noticeTitleCount.innerHTML = `${titleLength}`
+    if (titleLength > 50) {
+      noticeTitleCount.setAttribute("style", "color: red;")
+    }
+  })
+}
+
+window.addEventListener("turbo:load", countNotice)
+window.addEventListener("turbo:render", countNotice)
