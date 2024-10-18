47c53b26129a6246290a613a816415d805bcbe56
kimu805 <kimukimuok805@gmail.com>
2024-10-18 17:13:33 +0900

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @notice = FactoryBot.create(:notice, user_id: @user.id)
+    @comment = FactoryBot.build(:comment, user_id: @user.id, notice_id: @notice.id)
+  end
+
+  describe "コメント投稿機能" do
+    context "投稿できる場合" do
+      it "text, user_id, notice_idが適切に入力されていれば、投稿できる" do
+        expect(@comment).to be_valid
+      end
+    end
+    content "投稿できない場合" do
+      it "textが空では登録できない" do
+
+      end
+      it "textは1001文字以上では登録できない" do
+
+      end
+      it "user_idが紐づいていなければ登録できない" do
+
+      end
+      it "notice_idが紐づいていなければ登録できない" do
+
+      end
+    end
+  end
 end
