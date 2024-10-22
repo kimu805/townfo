b012168b32b74a62d280f73c01c0ef9df9e9ac4e
kimu805 <kimukimuok805@gmail.com>
2024-10-22 14:08:50 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_group
+  before_action :basic_auth
 
   private
   def configure_permitted_parameters
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   def current_group
     @current_group ||= current_user.groups.first
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "2222"
+    end
   end
 end
