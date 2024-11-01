6b1b41e0bc25c849bcb920fd187ec58d95a640c9
kimu805 <kimukimuok805@gmail.com>
2024-11-01 14:07:31 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
 
   def index
-    if current_user.groups.exists?
+    if current_user && current_user.groups.exists?
       redirect_to group_notices_path(current_group)
     end
   end
