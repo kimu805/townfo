919770f5621abc5e87f2387db45d827c37bf016e
kimu805 <kimukimuok805@gmail.com>
2024-11-08 11:26:53 +0900

    Update notice.rb

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,5 @@
 class Notice < ApplicationRecord
+  # association
   belongs_to :group
   belongs_to :user
   has_many :reads, as: :readable, dependent: :destroy
@@ -7,16 +8,23 @@ class Notice < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :tag
 
+  # validation
   with_options presence: true do
     validates :title, length: { maximum: 50 }
     validates :content, length: { maximum: 1000 }
     validates :tag_id, numericality: { other_than: 1, message:"を入力してください"}
   end
 
+  # method(for simple_calendar)
   def start_time
     schedule
   end
 
+  # scope
   scope :recent, -> { order("created_at DESC")}
 
+  # ransack
+  def self.ransackable_attributes(auth_object = nil)
+    
+  end
 end
