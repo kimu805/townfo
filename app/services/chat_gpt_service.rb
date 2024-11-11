cc5ff99bcb8047986313f2ba29c219df833b2446
kimu805 <kimukimuok805@gmail.com>
2024-11-11 15:16:39 +0900

    Create chat_gpt_service.rb

---
+++ app/services/chat_gpt_service.rb
@@ -0,0 +1,22 @@
+class ChatGptService
+  require "openai"
+
+  def initialize
+    @openai = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+  end
+
+  def chat(prompt)
+    response = @openai.completions(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "以下の条件で文章を作成してください"},
+          { role: "user", content: prompt }
+        ],
+        temperature: 0.7,
+        max_tokens: 500,
+      },
+    )
+    response["choices"].first["message"]["content"]
+  end
+end
