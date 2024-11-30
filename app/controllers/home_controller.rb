bad1e7bb0afec3e5be786a4499f61e54ec552fc0
kimu805 <kimukimuok805@gmail.com>
2024-11-30 10:18:55 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # ログインしているかつ、ユーザーがグループに所属している場合、グループのお知らせ一覧に遷移。
     if current_user && current_user.groups.exists?
       redirect_to group_notices_path(current_group)
     end
