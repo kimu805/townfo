de3d502e727ff7e8ba3bbe9c28727714dc7034dd
kimu805 <kimukimuok805@gmail.com>
2024-11-30 10:20:03 +0900

    Update memberships_controller.rb

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,6 +3,7 @@ class MembershipsController < ApplicationController
   before_action :set_group
   before_action :ensure_owner_or_myself
 
+  # グループを退会するメソッド
   def destroy
     membership_user = Membership.find(params[:id])
     membership_user.destroy
@@ -18,6 +19,7 @@ class MembershipsController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
+  # グループの管理者、または自分自身以外のアクセスを排除するメソッド
   def ensure_owner_or_myself
     set_group
     unless current_user.id == @group.owner_id || Membership.find(params[:id]).user == current_user
