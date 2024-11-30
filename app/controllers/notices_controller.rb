3c9d5d0b4b403bc8b6331bcaddcfc538658a64b5
kimu805 <kimukimuok805@gmail.com>
2024-11-30 10:30:42 +0900

    Update notices_controller.rb

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -2,11 +2,12 @@ class NoticesController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_notice, only: [:edit, :update, :destroy, :show, :mark_as_read]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :only_posted_person, only: [:edit, :update, :destroy]
 
   def index
     @q = current_group.notices.includes(:user).ransack(params[:q])
-    
+    # カレンダーからの日付指定がある場合は、日付に該当する投稿を表示。
+    # 日付指定がない場合は、検索結果に該当するすべての投稿を表示。
     if params[:schedule].present?
       @notices = current_group.notices.where(schedule: params[:schedule]).includes(:user).recent
     else
@@ -48,12 +49,14 @@ class NoticesController < ApplicationController
     @comments = @notice.comments.includes(:user)
   end
 
+  # 非同期で投稿に既読を付けるメソッド
   def mark_as_read
     current_user.reads.find_or_create_by(readable: @notice, complete: true)
     read_count = @notice.reads.count
     render json: {read_count: read_count}
   end
 
+  # 投稿の内容に対して、AIによる文章自動生成を行うメソッド
   def generate_content
     chat_gpt = ChatGptService.new
     generated_text = chat_gpt.chat(params[:prompt])
@@ -69,10 +72,11 @@ class NoticesController < ApplicationController
     @notice = current_group.notices.find(params[:id])
   end
 
-  def move_to_index
+  # 投稿した本人以外のアクセスを排除するメソッド
+  def only_posted_person
     set_notice
     unless current_user == @notice.user
-      redirect_to action: :index
+      redirect_to action: :show
     end
   end
 end
