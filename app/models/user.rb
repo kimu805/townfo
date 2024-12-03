a5b5998a0f85170f15126a5d1168182be85e9b51
kimu805 <kimukimuok805@gmail.com>
2024-12-04 08:35:03 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true, length: { maximum: 30 }
   validates :profile, length: { maximum: 1000 }
 
-  # def self.ransackable_attributes(auth_object = nil)
   #   ["nickname"]
   # end
 end
