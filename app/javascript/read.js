1cdc676d5c680fe66c74249e6ea4d59c904d31a8
kimu805 <kimukimuok805@gmail.com>
2024-12-09 17:20:30 +0900

    Update read.js

--- app/javascript/read.js
+++ app/javascript/read.js
@@ -8,11 +8,10 @@ function read() {
         // 100%表示されたとき (threshold: 1.0)
         if (entry.isIntersecting && entry.intersectionRatio === 1) {
           const noticeId = entry.target.dataset.noticeId;
-          const groupId = entry.target.dataset.groupId
 
           // 既読処理（表示された時点でサーバーにリクエストを送る）
           if (!readNotices.has(noticeId)) {
-            markNoticeAsRead(noticeId, groupId);
+            markNoticeAsRead(noticeId);
             readNotices.add(noticeId); // 重複送信を防ぐためにセットに追加
           }
         }
@@ -26,8 +25,8 @@ function read() {
 }
 
 // 既読情報をサーバーに送信する関数
-function markNoticeAsRead(noticeId, groupId) {
-  fetch(`/groups/${groupId}/notices/${noticeId}/mark_as_read`, {
+function markNoticeAsRead(noticeId) {
+  fetch(`/reads`, {
     method: 'POST',
     headers: {
       'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
